f09b48f7f0490a300d4b8147749b47f2978437ba
Pradeep <pradeep99909@gmail.com>
2024-03-26 23:29:37 +0530

    route for port folio and trade

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {
-    controllers
-} = require("../controllers");
+const controllers = require("../controllers");
 
 
-router.get("/portfolio", controllers.portfolioControlers.portfolio);
-router.get("/holdings", controllers.portfolioControlers.holdings);
-router.get("/returns", controllers.portfolioControlers.returns);
+router.get("/portfolio", controllers.portfolioControllers.portfolio);
+router.get("/holdings", controllers.portfolioControllers.holdings);
+router.get("/returns", controllers.portfolioControllers.returns);
 
 router.post("/addTrade", controllers.tradeControllers.addTrade);
 router.post("/updateTrade", controllers.tradeControllers.updateTrade);
