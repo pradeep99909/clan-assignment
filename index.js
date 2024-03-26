60772d0e56c8aa4da13a4ce4883b8898fc8879f5
Pradeep <pradeep99909@gmail.com>
2024-03-27 00:27:57 +0530

    env

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const { routes } = require("./api/routes");
 
+const mongodburl = process.env.MONGO_DB_URL;
+const port = process.env.PORT || 8080
 app.use(express.json({ urlencoded: false }));
 
 app.use("/api/v1", routes);
 
-const connection = mongoose.connect("mongodb://localhost:27017/assignment");
+const connection = mongoose.connect(process.env.MONGO_DB_URL);
 
 
 if(connection) {
    console.log("mongodb coonected!") 
 }
 
-app.listen(8080, () => console.log("App running on port 8080"))
+app.listen(port, () => console.log("App running on port 8080"))
